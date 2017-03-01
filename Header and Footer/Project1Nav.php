9f63c26aafa6e0f85419e3495d993c6225934cae
kdmnance <katie@nance.cc>
2017-02-28 22:55:58 -0700

    Working on nav

--- Header and Footer/Project1Nav.php
+++ Header and Footer/Project1Nav.php
@@ -4,8 +4,11 @@
 	<h1>Ingredients For You</h1>
 </div>
 
-	<ul class="nav nav-pills">
-		<li><a href="../Ingredient Pages/Lemongrass.php">Lemongrass</a></li>
-		<li><a href="../Ingredient Pages/Wasabi.php">Wasabi</a></li>
-		<li><a href="../Ingredient Pages/Capers.php">Capers</a></li>
+	<ul class="nav nav-pills nav-justified">
+		<li class="home-link"><a href="../index.php">Home</a></li>
+		<li class="lemongrass-link"><a href="../Ingredient Pages/Lemongrass.php">Lemongrass</a></li>
+		<li class="wasabi-link" ><a href="../Ingredient Pages/Wasabi.php">Wasabi</a></li>
+		<li class="capers-link"><a href="../Ingredient Pages/Capers.php">Capers</a></li>
+		<li class="lemongrass-link"><a href="../about.php">About Us</a></li>
+
 	</ul>
--- Ingredient Pages/Lemongrass.php
+++ Ingredient Pages/Lemongrass.php
@@ -4,8 +4,11 @@
 ?>
 
 <body>
-<?php
-	include "../Header and Footer/Project1Nav.php";
-?>
+	<?php
+		include "../Header and Footer/Project1Nav.php";
+	?>
+	<div class="col-md-2 hidden-sm hidden-xs sidebar lemongrass-sidebar"></div>
+	<div class="col-md-8 content">more content</div>
+	<div class="col md-2 hidden-sm hidden-xs sidebar lemongrass-sidebar"></div>
 </body>
 </html>

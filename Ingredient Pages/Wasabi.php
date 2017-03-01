8d536007c62a6055730e6454f565f4a5ac101ba1
Courtney <cschulze@colostate.edu>
2017-03-01 00:12:09 -0700

    Basic layout for wasabi

--- Ingredient Pages/Wasabi.php
+++ Ingredient Pages/Wasabi.php
@@ -1,10 +1,19 @@
 <?php  
-   $pageTitle = 'Wasabi';
-   include '../Header and Footer/Project1Header.php';
+$pageTitle = 'Wasabi';
+include '../Header and Footer/Project1Header.php';
 ?>
 
 <body>
-	
-	<?php include '../Header and Footer/Project1Nav.php' ?>
-	
+	<?php
+	include "../Header and Footer/Project1Nav.php";
+	?>
+	<div class="col-md-2 hidden-sm hidden-xs sidebar lemongrass-sidebar"></div>
+	<div class="col-md-8 content">
+		<h1>Wasabi</h1>
+		<p>Stuff here</p>
+		<div class="image"></div>
+	</div>
+	<div class="col md-2 hidden-sm hidden-xs sidebar lemongrass-sidebar"></div>
 </body>
+</html>
+	

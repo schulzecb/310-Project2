ce9a64e65b891a71e24289b0ac3dddd700054573
kdmnance <katie@nance.cc>
2017-02-28 21:42:07 -0700

    Working on CSS

--- Header and Footer/Project1Header.php
+++ Header and Footer/Project1Header.php
@@ -23,6 +23,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 	integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Covered+By+Your+Grace|Dosis" rel="stylesheet">
 	<link rel="stylesheet" href="../project1.css">
 
 </head>
---
+++ Ingredient Pages/Lemongrass.php
@@ -0,0 +1,11 @@
+<?php  
+   $pageTitle = 'Lemongrass';
+   include '../Header and Footer/Project1Header.php';
+?>
+
+<body>
+<?php
+	include "../Header and Footer/Project1Nav.php";
+?>
+</body>
+</html>

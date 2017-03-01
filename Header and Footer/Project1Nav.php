e6e43e166fded0ac4dd1c1729cfa733bd937b401
kdmnance <katie@nance.cc>
2017-02-28 22:03:12 -0700

    Working on nav

--- Header and Footer/Project1Nav.php
+++ Header and Footer/Project1Nav.php
@@ -3,3 +3,9 @@
 <div class="jumbotron">
 	<h1>Ingredients For You</h1>
 </div>
+
+	<ul class="nav nav-pills">
+		<li><a href="../Ingredient Pages/Lemongrass.php">Lemongrass</a></li>
+		<li><a href="../Ingredient Pages/Wasabi.php">Wasabi</a></li>
+		<li><a href="../Ingredient Pages/Capers.php">Capers</a></li>
+	</ul>

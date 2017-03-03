347938db494736bb39e30753d110337579977178
Katie Nance <katie@nance.cc>
2017-03-03 13:05:55 -0700

    Working on some fixes

--- Header and Footer/Project1Header.php
+++ Header and Footer/Project1Header.php
@@ -24,9 +24,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Covered+By+Your+Grace|Dosis" rel="stylesheet">
-	<script src="https://use.fontawesome.com/71f0c450ac.js"></script>
 	<link rel="stylesheet" href="../project1.css">
-	<link rel="stylesheet" href="./project1.css">
 
 </head>
 <body>
--- Login/Login.php
+++ Login/Login.php
@@ -4,17 +4,25 @@
 <ul class="list-group">
   <li class="list-group-item comment"><span class="username">Username</span> <span class="message"><br><hr>Message here</span></li>
     <li class="list-group-item comment"><span class="username">Username</span> <span class="message"><br><hr>Message here</span></li>
+	<li class="list-group-item comment"><span class="username">Username</span> <span class="message"><br><hr>Message here</span></li>
+    <li class="list-group-item comment"><span class="username">Username</span> <span class="message"><br><hr>Message here</span></li>
+	<li class="list-group-item comment"><span class="username">Username</span> <span class="message"><br><hr>Message here</span></li>
+    <li class="list-group-item comment"><span class="username">Username</span> <span class="message"><br><hr>Message here</span></li>
 
 </ul>
+<form action="Message.php" method="post">
+	<textarea class="form-control" rows="3" placeholder="Write your comment..."></textarea>
+	<button class="btn btn-default" role="button" type="submit">Submit</button><span class="signed-in">Signed in as: username</span>
+</form>
+
 <!--If not signed in -->
 <h3>Sign in</h3>
-<div class="input-group">
-  <span class="input-group-addon" id="username"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>
+<form action="Login.php" method="post">
+<div class="form-group">
   <input type="text" class="form-control" placeholder="Username" aria-describedby="username">
 </div>
-<div class="input-group">
-  <span class="input-group-addon" id="pass"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></span>
+<div class="form-group">
   <input type="password" class="form-control" placeholder="Password" aria-describedby="pass">
 </div>
-<br>
-<a href="#" class="btn btn-default" role="button">Sign in</a>
+<button class="btn btn-default" role="button" type="submit">Sign in</button>
+</form>

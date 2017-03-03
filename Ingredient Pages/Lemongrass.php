9fd528d76c417329e41900a5b303075818c727e6
Katie Nance <katie@nance.cc>
2017-03-02 18:21:29 -0700

    Fixing css

--- Ingredient Pages/Lemongrass.php
+++ Ingredient Pages/Lemongrass.php
@@ -7,7 +7,8 @@
 	<?php
 		include "../Header and Footer/Project1Nav.php";
 	?>
-	<div class="col-md-2 hidden-sm hidden-xs sidebar lemongrass-sidebar"></div>
+	<div class="container-fluid lemongrass-container">
+	<div class="col-md-2 hidden-sm hidden-xs sidebar"></div>
 	<div class="col-md-8 content">
 		<h1>Lemongrass</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique scelerisque ligula. Aenean in mattis eros, a semper magna. Proin tortor purus, gravida quis finibus sit amet, malesuada a mi. Sed pellentesque quam at vulputate pharetra. Praesent non erat faucibus, commodo turpis et, suscipit eros. Aliquam tincidunt quis enim ut porttitor. Phasellus porta sodales magna. Vivamus tellus mi, mollis vitae nunc nec, accumsan auctor tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam nec risus sodales, faucibus lacus sit amet, faucibus risus. Morbi et ornare nulla, et consectetur leo. Mauris interdum tempor velit, in feugiat ipsum commodo non.</p>
@@ -16,8 +17,8 @@
 		include "../Login/Login.php";
 	?>
 	</div>
-	<div class="col md-2 hidden-sm hidden-xs sidebar lemongrass-sidebar"></div>
-	
+	<div class="col md-2 hidden-sm hidden-xs sidebar"></div>
+	</div>
 	<?php
 		include "../Header and Footer/Project1Footer.php";
 	?>

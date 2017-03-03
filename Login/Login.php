c7ef58c30edadd3e598de5aed52f921f63162b97
Katie Nance <katie@nance.cc>
2017-03-03 15:05:13 -0700

    Working on login

--- Login/Login.php
+++ Login/Login.php
@@ -1,30 +1,36 @@
 <?php  
    $pageTitle = 'Sign In';
    include '../Header and Footer/Project1Header.php';
+   include "../Header and Footer/Project1Nav.php";
+   include "./Session.php";
 ?>
 
-	<?php
-		include "../Header and Footer/Project1Nav.php";
-	?>
-	<div class="container-fluid wasabi-container">
+<div class="container-fluid wasabi-container">
 	<div class="col-md-2 hidden-sm hidden-xs sidebar"></div>
 	<div class="col-md-8 content">
-<!--If not signed in -->
-<h2>Sign in</h2>
-<form action="Login.php" method="post">
-<div class="form-group">
-  <input type="text" class="form-control" placeholder="Username" aria-describedby="username">
-</div>
-<div class="form-group">
-  <input type="password" class="form-control" placeholder="Password" aria-describedby="pass">
-</div>
-<button class="btn btn-default" role="button" type="submit">Sign in</button>
-</form>
+		<?php
+			if (!isset($_SESSION["username"])){
+				echo '<h2>Sign in</h2>';
+				echo '<form action="Login.php" method="post">';
+				echo '	<div class="form-group">';
+				echo '		<input type="text" class="form-control" placeholder="Username" aria-describedby="username">';
+				echo '	</div>';
+				echo '	<div class="form-group">';
+				echo '		<input type="password" class="form-control" placeholder="Password" aria-describedby="pass">';
+				echo '	</div>';
+				echo '	<button class="btn btn-default" role="button" type="submit">Sign in</button>';
+				echo '</form>';
+			}
+			else {
+				echo '<p>You are signed in as $_SESSION["username"].</p>';
+				header('Location: $_SESSION["lastpage]');
+			}
+		?>
 	</div>
 	<div class="col md-2 hidden-sm hidden-xs sidebar"></div>
-	</div>
-	<?php
-		include "../Header and Footer/Project1Footer.php";
-	?>
+</div>
+<?php
+	include "../Header and Footer/Project1Footer.php";
+?>
 
 

b340081bef374048d5b0ca8ab828bd534e11dcba
Katie Nance <katie@nance.cc>
2017-03-06 17:23:14 -0700

    Adding login/logout link in footer

--- Header and Footer/Project1Footer.php
+++ Header and Footer/Project1Footer.php
@@ -1,5 +1,14 @@
 <div class="footer">
-<p>This site is part of a CSU <a href="http://cs.colostate.edu/~ct310">CT 310</a> Course Project. </p>
+<p><?php 
+    if (isset($_SESSION["username"])){
+        echo 'Signed in as <a href="../Login/Login.php">' . $_SESSION["username"] . '</a> | ';
+    }
+    else {
+        echo '<a href="../Login/Login.php">Sign in</a> | ';
+    }
+?> 
+
+This site is part of a CSU <a href="http://cs.colostate.edu/~ct310">CT 310</a> Course Project. </p>
 </div>
 
 </body>

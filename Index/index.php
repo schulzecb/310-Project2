8ef6084cdf7e2e44d0c604675cf811a2f1dd6a31
jcollera <jakecolleran@hotmail.com>
2017-04-10 10:27:42 -0600

    Update index.php

--- Index/index.php
+++ Index/index.php
@@ -6,6 +6,7 @@
 ?>
 
 	<?php
+	$_SESSION['control'] = 0;
 		include "../Header and Footer/Nav.php";
 	?>
 	<div class="container-fluid">
@@ -15,6 +16,7 @@
 	
 	
         <?php
+        $_SESSION['prevURL'] = $_SERVER['REQUEST_URI'];
             //display ingredients in database
             require_once '../lib/Database.php';
             $db = new Database();
@@ -24,11 +26,12 @@
                 <div class="row" style="text-align: center; padding: 10px; margin-bottom: 2px;">
                     <div class="col-xs-12">
                     <h3>
-                       <?php echo "<a href= 'https://www.cs.colostate.edu/~cschulze/310-Project2/310-Project2/Ingredient Pages/DisplayIngredient.php?ing=" . $ing["ingredient_name"] . "'>"; ?>
+                       <?php echo "<a href= 'https://www.cs.colostate.edu/~jcollera/310-Project2/Ingredient Pages/DisplayIngredient.php?ing=" . $ing["ingredient_name"] . "'>"; ?>
                        <?php echo $ing["ingredient_name"];?>
                        </a>
                     </h3>
-                        <img style="height: 200px; width: 200px; margin-bottom: 5px;" alt="<?php echo $ing["ingredient_name"];?>" />
+                    <?php $img = "../Ingredient Pages/".$ing["image"];?>
+                        <img src= "<?php echo $img?>" style="height: 200px; width: 200px; margin-bottom: 5px;" alt="<?php echo $ing["ingredient_name"];?>" />
                     </div>
 		</div>
             <?php }; ?>	
